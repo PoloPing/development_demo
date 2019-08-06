24eca637a611db9588da534d4d24a5201604bf6c
poloChen <polo@unnotech.com>
2019-08-06 14:44:57 +0800

    ['feature'] - build docker-compose enviroment ['feature'] - add mariadb

--- development_demo/settings.py
+++ development_demo/settings.py
@@ -75,11 +75,18 @@ WSGI_APPLICATION = 'development_demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_SERVICE'),
+        'PORT': 3306,
+        'DEFAULT-CHARACTER-SET': 'utf8',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
+python manage.py makemigrations
+until python manage.py migrate; do
+  sleep 2
+  echo "Retry!";
+done
 python manage.py runserver 0.0.0.0:8000
